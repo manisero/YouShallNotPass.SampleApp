af06b43ff970095ef262007913310741d0cdf4d8
manisero <michalaniserowicz@gmail.com>
2017-11-25 10:57:31 +0100

    Added YouShallNotPass

--- src/Manisero.YouShallNotPass.SampleApp/AppGateway.cs
+++ src/Manisero.YouShallNotPass.SampleApp/AppGateway.cs
@@ -12,20 +12,36 @@ namespace Manisero.YouShallNotPass.SampleApp
     {
         public static readonly AppGateway Instance = new AppGateway();
 
+        private readonly IValidationEngine _validationEngine;
+
         private readonly IDictionary<object, object> _commandHandlers = new Dictionary<object, object>
         {
             [typeof(CreateUserCommand)] = new CreateUserCommandHandler()
         };
 
+        public AppGateway()
+        {
+            _validationEngine = new ValidationEngineBuilder()
+                .RegisterValidationRule(typeof(CreateUserCommand), CreateUserCommand.ValidationRule)
+                .Build();
+        }
+
         public CommandResult Handle<TCommand>(TCommand command)
         {
+            var validationResult = _validationEngine.Validate(command);
+
+            if (validationResult.HasError())
+            {
+                return new CommandResult
+                {
+                    ValidationError = validationResult.Error
+                };
+            }
+
             var handler = (ICommandHanlder<TCommand>)_commandHandlers[typeof(TCommand)];
             handler.Handle(command);
 
-            return new CommandResult
-            {
-                ValidationError = "Invalid"
-            };
+            return new CommandResult();
         }
     }
 }
--- src/Manisero.YouShallNotPass.SampleApp/Commands/CreateUserCommand.cs
+++ src/Manisero.YouShallNotPass.SampleApp/Commands/CreateUserCommand.cs
@@ -1,10 +1,30 @@
-﻿namespace Manisero.YouShallNotPass.SampleApp.Commands
+﻿using System.Collections.Generic;
+using Manisero.YouShallNotPass.Validations;
+
+namespace Manisero.YouShallNotPass.SampleApp.Commands
 {
     public class CreateUserCommand
     {
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public static ComplexValidationRule<CreateUserCommand> ValidationRule = new ComplexValidationRule<CreateUserCommand>
+        {
+            MemberRules = new Dictionary<string, IValidationRule>
+            {
+                [nameof(Email)] = new AllValidationRule<string>
+                {
+                    Rules = new List<IValidationRule<string>>
+                    {
+                        new NotNullValidationRule<string>(),
+                        new EmailValidationRule()
+                    }
+                },
+                [nameof(FirstName)] = new NotNullNorWhiteSpaceValidationRule(),
+                [nameof(LastName)] = new NotNullNorWhiteSpaceValidationRule()
+            }
+        };
     }
 
     public class CreateUserCommandHandler : ICommandHanlder<CreateUserCommand>
